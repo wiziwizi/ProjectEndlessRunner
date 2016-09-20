52d526dbf75514432cef08c31e2951b57c3235b9
IanMunster <I.Munster92@gmail.com>
2016-09-20 10:26:29 +0200

    20-09-16 Movement Start

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	[SerializeField] private int m_maxSpeed;
+	private Rigidbody2D rigid;
+	private float moveX = 0f;
+	private float moveY = 0f;
+	private bool onGround = true;
+
+	// Use this for initialization
+	void Start () {
+		rigid = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveX = Input.GetAxis ("Horizontal");
+		moveY = Input.GetButton ("Fire0");
+		rigid.velocity = new Vector2 (moveX*m_maxSpeed, 0f);
+	}
+}
