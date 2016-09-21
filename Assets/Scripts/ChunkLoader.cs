b6f46dd930f1060620244e8af4650a8600644ee8
Rico Evers <ricoevers@live.nl>
2016-09-21 16:26:50 +0200

    ChunkLoader/graybox en test array

---
+++ Assets/Scripts/ChunkLoader.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChunkLoader : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject[] LevelChunks;
+	private Vector3 BGSize = new Vector3(50,0,0);
+	private int RandomNumber;
+	private int OldRandomNumber;
+
+	void Start()
+	{
+		RandomNumber = Random.Range (0, 2);
+		LevelChunks[RandomNumber].SetActive (true);
+		OldRandomNumber = RandomNumber;
+	}
+
+	void OnBecameInvisible()
+	{
+		LevelChunks[OldRandomNumber].SetActive (false);
+		RandomNumber = Random.Range (0, 2);
+		Debug.Log (RandomNumber);
+		transform.position += BGSize;
+		LevelChunks[RandomNumber].SetActive (true);
+		OldRandomNumber = RandomNumber;
+	}
+}
