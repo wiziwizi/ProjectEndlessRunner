732495ca96828692120a03e44d4ab2b35128d8c9
Rico Evers <ricoevers@live.nl>
2016-09-28 13:24:55 +0200

    menu manager/ paralax background / platform art

---
+++ Assets/Scripts/MenuManagement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuManagement : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject[] canvas;
+
+	// Use this for initialization
+	void Start ()
+	{
+		for(int i = 0; i < canvas.Length; i++)
+		{
+			canvas [i].SetActive (false);
+		}
+		canvas [0].SetActive (true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnButtonPress (GameObject canvasID)
+	{
+		Debug.Log ("Hallo");
+	}
+	public void LoadScene (int levelNumber)
+	{
+		SceneManager.LoadScene (levelNumber);
+	}
+}
