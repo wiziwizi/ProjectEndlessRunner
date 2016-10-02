37e72a9f1c546b670428c4d353418fa160b188c5
IanMunster <I.Munster92@gmail.com>
2016-10-02 14:33:38 +0200

    02-10-16 PickUps
    Made pickup spawner & remover
    working on ObjectPooler & PickUp Collecter

--- Assets/Scripts/PickUpCollecter.cs
+++ Assets/Scripts/PickUpCollecter.cs
@@ -3,16 +3,11 @@ using System.Collections;
 
 public class PickUpCollecter : MonoBehaviour {
 
-	[SerializeField] private GameObject[] allPickUps;
-	private int allPickUpsCount;
+	private GameObject collectedPickUp;
 
 	// Use this for initialization
 	void Start () {
-		foreach(GameObject pickUp in allPickUps){
-			allPickUpsCount++;
-			//print (allPickUpsCount);
-		}
-		allPickUps = new GameObject[allPickUpsCount];
+
 	}
 	
 	// Update is called once per frame
