12ce645adc263cec7a8b4ca3b967fd9a72db74b0
Rico Evers <ricoevers@live.nl>
2016-10-07 09:23:17 +0200

    healthScript/End Scene

--- Assets/Scripts/OuOfBounds.cs
+++ Assets/Scripts/OuOfBounds.cs
@@ -3,15 +3,22 @@ using System.Collections;
 
 public class OuOfBounds : MonoBehaviour {
 
-	[SerializeField]
-	private string tagOfObject;
+	[SerializeField] private string tagOfObject;
+	private Health lifeScript;
+
+
+	void Awake()
+	{
+		
+	}
 
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.gameObject.CompareTag(tagOfObject))
 		{
-			Debug.Log ("trigger");
-			//LoseLive ();
+			Debug.Log ("y");
+			lifeScript = other.GetComponent<Health>();
+			lifeScript.LoseLife (1);
 		}
 	}
 }
---
+++ Assets/Scripts/Player/Health.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+
+	[SerializeField] private int health;
+	[SerializeField] private string otherPlayerName;
+
+	private SceneLoader sceneLoader;
+	[SerializeField] private GameObject sceneLoaderObject;
+
+	private Vector3 otherPlayerPosition;
+
+	void Awake()
+	{
+		sceneLoader = sceneLoaderObject.GetComponent<SceneLoader>();
+		otherPlayerPosition = GameObject.Find(otherPlayerName).transform.position;
+	}
+
+	public void LoseLife(int retract)
+	{
+		health -= retract;
+
+		transform.position = new Vector3 (otherPlayerPosition.x, 20, otherPlayerPosition.z);
+
+		if(health <= 0)
+		{
+			StartCoroutine (End());
+			transform.position = new Vector3 (otherPlayerPosition.x, otherPlayerPosition.y, otherPlayerPosition.z);
+			Time.timeScale = 0.1f;
+		}
+	}
+
+	IEnumerator End()
+	{
+		yield return new WaitForSeconds (0.5f);
+		if(sceneLoader != null)
+		{
+			Time.timeScale = 1;
+			sceneLoader.LoadScene (2);
+		}
+	}
+}
