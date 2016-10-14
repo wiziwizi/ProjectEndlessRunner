648d217c53e0176991f74e76bfb45ad57ffcb977
IanMunster <I.Munster92@gmail.com>
2016-10-15 00:30:50 +0200

    ExitApplication

---
+++ Assets/ExitApplication.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitApplication : MonoBehaviour {
+
+	public void OnButtonPress(){
+		Screen.fullScreen = false;
+		Application.Quit ();
+	}
+}
